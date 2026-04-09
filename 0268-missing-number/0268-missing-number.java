dac146152ea8710dea4be5631d99d2f606862b06
Prakash Kumar <prakashk13624@gmail.com>
2026-04-09 18:54:33 +0530

    Time: 1 ms (29.27%), Space: 47.4 MB (39.1%) - LeetHub

--- 0268-missing-number/0268-missing-number.java
+++ 0268-missing-number/0268-missing-number.java
@@ -3,13 +3,22 @@ class Solution
     public int missingNumber(int[] nums) 
     {
         int n = nums.length;
-        long expectedSum = n*(n+1)/2;
-
-        long actualSum = 0;
-        for(int i = 0; i < n; i++)
-        {
-            actualSum += nums[i];
+        int i = 0;
+        while(i < nums.length) {
+            if(nums[i] == i || nums[i] == n) i++;
+            else {
+                int idx = nums[i];
+                swap(nums, i ,idx);
+            }
         }
-        return (int)(expectedSum - actualSum);
+        for(int j = 0; j < nums.length; j++) {
+            if(nums[j] != j) return j;
+        }
+        return n;
+    }
+    void swap(int[] nums, int i, int idx) {
+        int temp = nums[i];
+        nums[i] = nums[idx];
+        nums[idx] = temp;
     }
 }
