153b941b9ed7dcace0fcf7f9c7b83364ca222325
Prakash Kumar <prakashk13624@gmail.com>
2026-04-09 13:43:21 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.java
+++ 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.java
@@ -1,11 +1,9 @@
-class Solution 
-{
-    public boolean searchMatrix(int[][] matrix, int target) 
-    {
-        int totalRow = matrix.length, totalCol = matrix[0].length;
-        int i = 0, j = totalCol-1;
-        while(i<totalRow && j>=0)
-        {
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int n = matrix.length;
+        int i = 0;
+        int j = n-1;
+        while(i<n-1 || j>0) {
             if(matrix[i][j] == target) return true;
             else if(matrix[i][j] < target) i++;
             else j--;
