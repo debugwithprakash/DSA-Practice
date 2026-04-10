80f40e8ceb2c8aba21257cc9385defa720bc39d9
Prakash Kumar <prakashk13624@gmail.com>
2026-04-10 12:19:44 +0530

    Time: 1 ms (89%), Space: 43.5 MB (8.89%) - LeetHub

--- 0520-detect-capital/0520-detect-capital.java
+++ 0520-detect-capital/0520-detect-capital.java
@@ -1,11 +1,7 @@
-class Solution 
-{
-    public boolean detectCapitalUse(String word) 
-    {
-        int len = word.length();
-        int count = 0;
-        for(int i = 0; i < len; i++)
-        {
+class Solution {
+    public boolean detectCapitalUse(String word) {
+        int count = 0, len = word.length();
+        for(int i = 0; i < len; i++) {
             if(Character.isUpperCase(word.charAt(i))) count++;
         }
         return count == len || count == 0 || (count == 1 && Character.isUpperCase(word.charAt(0)));
