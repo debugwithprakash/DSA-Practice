233953f153c367731bd256c9c54213fcc781cbf0
Prakash Kumar <prakashk13624@gmail.com>
2026-05-23 20:22:42 +0530

    Time: 0 ms (100%), Space: 42.9 MB (52.38%) - LeetHub

--- 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.java
+++ 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.java
@@ -10,25 +10,25 @@
  */
 class Solution {
     public ListNode middleNode(ListNode head) {
-        // ListNode slow = head;
-        // ListNode fast = head;
-        // while(fast!=null && fast.next!=null) {
-        //     slow = slow.next;
-        //     fast = fast.next.next;
+        ListNode slow = head;
+        ListNode fast = head;
+        while(fast!=null && fast.next!=null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+        
+        
+        // int length = 0;
+        // ListNode temp = head;
+        // while(temp != null) {
+        //     temp = temp.next;
+        //     length++;
         // }
-        // return slow;
-        
-        
-        int length = 0;
-        ListNode temp = head;
-        while(temp != null) {
-            temp = temp.next;
-            length++;
-        }
-        temp = head;
-        for(int i = 0; i < length/2; i++) {
-            temp = temp.next;
-        }
-        return temp;
+        // temp = head;
+        // for(int i = 0; i < length/2; i++) {
+        //     temp = temp.next;
+        // }
+        // return temp;
     }
 }
