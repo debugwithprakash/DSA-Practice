deeab695183bb0146ec25f3b782daf42093131f8
Prakash Kumar <prakashk13624@gmail.com>
2026-05-24 15:35:37 +0530

    Time: 1 ms (99.96%), Space: 52.9 MB (25.91%) - LeetHub

--- 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.java
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.java
@@ -13,43 +13,37 @@ public class Solution {
     public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
         // Finding Length of 1st LL
         int lengthA = 0;
-        ListNode temp = headA;
-        while(temp!=null) {
-            temp = temp.next;
+        ListNode tempA = headA;
+        while(tempA!=null) {
+            tempA = tempA.next;
             lengthA++;
         }
         // Finding Length of 2nd LL
-        temp = headB;
         int lengthB = 0;
-        while(temp!=null) {
-            temp = temp.next;
+        ListNode tempB = headB;
+        while(tempB!=null) {
+            tempB = tempB.next;
             lengthB++;
         }
+        // Set At equal Position
+        tempA = headA;
+        tempB = headB;
         if(lengthA>=lengthB) {
-            ListNode tempA = headA;
             for(int i = 0; i < lengthA-lengthB; i++) {
                 tempA = tempA.next;
             }
-
-            ListNode tempB = headB;
-            while(tempA != tempB) {
-                tempA = tempA.next;
-                tempB = tempB.next;
-            }
-            return tempA;
         }
         else {
-            ListNode tempB = headB;
             for(int i = 0; i < lengthB-lengthA; i++) {
                 tempB = tempB.next;
             }
-
-            ListNode tempA = headA;
-            while(tempA != tempB) {
-                tempA = tempA.next;
-                tempB = tempB.next;
-            }
-            return tempB;
         }
+        // Increase while don't meet at same node
+        while(tempA != tempB) {
+            tempA = tempA.next;
+            tempB = tempB.next;
+        }
+
+        return tempA;
     }
 }
