a894178b4de040dbd2aaa67cb949019b0b40ac98
Prakash Kumar <prakashk13624@gmail.com>
2026-07-21 10:50:18 +0530

    Time: 13 ms (31.63%), Space: 55.3 MB (69.1%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,21 +1,15 @@
 class Solution {
     public int majorityElement(int[] nums) {
-        int n = nums.length;
-        int freq = 0;
-        int ans = 0;
-
-        for(int i = 0; i < n; i++) {
-            if(freq == 0) {
-                ans = nums[i];
-            }
-            if(ans == nums[i]) {
-                freq++;
-            }
-            else {
-                freq--;
+        Stack<Integer> st = new Stack<>();
+        for(int i = 0; i < nums.length; i++) {
+            if(st.isEmpty()) {
+                st.push(nums[i]);
+            } else if(st.peek()!=nums[i]) {
+                st.pop();
+            } else {
+                st.push(nums[i]);
             }
         }
-
-        return ans;
+        return st.peek();
     }
 }
