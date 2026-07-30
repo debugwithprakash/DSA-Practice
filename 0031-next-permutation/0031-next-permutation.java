762235a83309eaa557cd6123fa6c9dc10382033c
Prakash Kumar <prakashk13624@gmail.com>
2026-07-30 08:32:19 +0530

    Time: 0 ms (100%), Space: 44.8 MB (16.09%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.java
+++ 0031-next-permutation/0031-next-permutation.java
@@ -1,44 +1,37 @@
 class Solution {
     public void nextPermutation(int[] nums) {
         int n = nums.length;
-        int k = -1;
-
+        // Step-1: finding break Idx
+        int breakIdx = -1;
         for(int i = n-2; i >= 0; i--) {
-            if(nums[i] < nums[i+1]) {
-                k = i;
+            if(nums[i]<nums[i+1]) {
+                breakIdx = i;
                 break;
             }
         }
-        // Edge case
-        if(k == -1) {
-            int a = 0;
-            int b = n-1;
-            while(a < b) {
-                swap(nums, a, b);
-                a++;
-                b--;
-            }
+        if(breakIdx == -1) {
+            reverse(nums, 0, n-1);
             return;
         }
-
-        for(int i = n-1; i > k; i--) {
-            if(nums[i] > nums[k]) {
-                swap(nums, k, i);
+        // Step-2: finding slightly greater Idx and swapping
+        for(int i = n-1; i >= breakIdx; i--) {
+            if(nums[i]>nums[breakIdx]) {
+                int temp = nums[i];
+                nums[i] = nums[breakIdx];
+                nums[breakIdx] = temp;
                 break;
             }
         }
-
-        int a = k+1;
-        int b = n-1;
+        // Step-3: reverse breakIdx+1 to n-1
+        reverse(nums, breakIdx+1, n-1);
+    }
+    public void reverse(int[] nums, int a, int b) {
         while(a < b) {
-            swap(nums, a, b);
+            int temp = nums[a];
+            nums[a] = nums[b];
+            nums[b] = temp;
             a++;
             b--;
         }
-    }
-    public void swap(int[] nums, int a, int b) {
-        int temp = nums[a];
-        nums[a] = nums[b];
-        nums[b] = temp;
     }
 }
