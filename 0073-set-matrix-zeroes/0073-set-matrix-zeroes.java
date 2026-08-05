a805a8d1b0d49926fb3a3b0f23614891ca84635d
Prakash Kumar <prakashk13624@gmail.com>
2026-08-05 22:18:56 +0530

    Time: 1 ms (97.35%), Space: 47.5 MB (81.52%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -3,30 +3,40 @@ class Solution {
         int m = matrix.length;
         int n = matrix[0].length;
 
-        // Creating two Arrays and filling them by 1
-        int[] row = new int[n];
-        int[] col = new int[m];
-        Arrays.fill(row, 1);
-        Arrays.fill(col, 1);
-
-        // Marking in row and column arrays
+        // Marking row and column which have Zero
+        int col0 = 1;
         for(int i = 0; i < m; i++) {
             for(int j = 0; j < n; j++) {
                 if(matrix[i][j] == 0) {
-                    row[j] = 0;
-                    col[i] = 0;
+                    matrix[i][0] = 0;
+                    if(j != 0) 
+                        matrix[0][j] = 0;
+                    else
+                        col0 = 0;
                 }
             }
         }
-        
-        // Seting Zeroes in Matrix
-        for(int i = 0; i < m; i++) {
-            for(int j = 0; j < n; j++) {
+
+        //Setting Zeroes in matrix
+        for(int i = 1; i < m; i++) {
+            for(int j = 1; j < n; j++) {
                 if(matrix[i][j] != 0) {
-                    if(row[j] == 0 || col[i] == 0) {
+                    if(matrix[0][j] == 0 || matrix[i][0] == 0) {
                         matrix[i][j] = 0;
                     }
                 }
+            }
+        }
+        //Setting Zeroes in first row
+        if(matrix[0][0] == 0) {
+            for(int j = 1; j < n; j++) {
+                matrix[0][j] = 0;
+            }
+        }
+        //Setting Zeroes in first column
+        if(col0 == 0) {
+            for(int i = 0; i < m; i++) {
+                matrix[i][0] = 0;
             }
         }
     }
