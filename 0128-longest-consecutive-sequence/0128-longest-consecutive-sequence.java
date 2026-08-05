24ad77a9237c0222f81ff9ed9ebf91864e852102
Prakash Kumar <prakashk13624@gmail.com>
2026-08-05 07:28:09 +0530

    Time: 35 ms (18.93%), Space: 95.6 MB (52.03%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.java
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.java
@@ -2,37 +2,29 @@ class Solution {
     public int longestConsecutive(int[] nums) {
         int n = nums.length;
         if(n == 0) return 0;
-        if(n == 1) return 1;
 
-        // adding elements in TreeSet
-        TreeSet<Integer> set = new TreeSet<>();
+        // Putting all elements in set
+        HashSet<Integer> set = new HashSet<>();
         for(int ele : nums) {
             set.add(ele);
         }
 
-        // TreeSet to array
-        int[] arr = new int[set.size()];
-        int idx = 0;
+        // Finding longest consecutive Sequence
+        int longestSeq = 0;
         for(int ele : set) {
-            arr[idx++] = ele;
-        }
+            if(!set.contains(ele-1)) {
+                int currEle = ele;
+                int currSeq = 1;
 
-        // Finding Longest consecutive seq using two pointer
-        int maxLen = 1;
-        int i = 0, j = 1;
-        while(j < arr.length) {
-            if(arr[j] == arr[j-1]+1) {
-                j++;
-            } else {
-                int len = j-i;
-                maxLen = Math.max(maxLen, len);
-                i = j;
-                j++;
+                while(set.contains(currEle + 1)) {
+                    currEle++;
+                    currSeq++;
+                }
+
+                longestSeq = Math.max(longestSeq, currSeq);
             }
         }
-        int len = j-i;
-        maxLen = Math.max(maxLen, len);
 
-        return maxLen;
+        return longestSeq;
     }
 }
