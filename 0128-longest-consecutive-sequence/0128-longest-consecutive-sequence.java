520a15c32b098700543d85044df9716ccdd9fa5e
Prakash Kumar <prakashk13624@gmail.com>
2026-08-05 07:13:06 +0530

    Time: 80 ms (5.04%), Space: 92.4 MB (76.27%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.java
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.java
@@ -4,20 +4,23 @@ class Solution {
         if(n == 0) return 0;
         if(n == 1) return 1;
 
+        // adding elements in TreeSet
         TreeSet<Integer> set = new TreeSet<>();
         for(int ele : nums) {
             set.add(ele);
         }
 
-        int[] arr = new int[n];
+        // TreeSet to array
+        int[] arr = new int[set.size()];
         int idx = 0;
         for(int ele : set) {
             arr[idx++] = ele;
         }
 
+        // Finding Longest consecutive seq using two pointer
         int maxLen = 1;
         int i = 0, j = 1;
-        while(j < n) {
+        while(j < arr.length) {
             if(arr[j] == arr[j-1]+1) {
                 j++;
             } else {
