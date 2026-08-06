ddaf71d78095ba000c8893d7ad010c5e90d1eb01
Prakash Kumar <prakashk13624@gmail.com>
2026-08-07 00:27:57 +0530

    Time: 24 ms (76.42%), Space: 48.3 MB (97.1%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
@@ -1,22 +1,19 @@
-class Solution 
-{
-    public int subarraySum(int[] nums, int k) 
-    {
-        HashMap<Integer, Integer> hm = new HashMap<>();
-
+class Solution {
+    public int subarraySum(int[] nums, int k) {
         int prefixSum = 0;
-        hm.put(0, 1); 
-        int noOfSubArrayPossible = 0;
-        for (int i = 0; i < nums.length; i++) {
-            prefixSum += nums[i];               
-            int remove = prefixSum - k;
-            
-            if(hm.containsKey(remove)){         
-                noOfSubArrayPossible += hm.get(remove);
+        int count = 0;
+        HashMap<Integer, Integer> map = new HashMap<>();
+        map.put(0, 1);
+        for(int ele : nums) {
+            prefixSum += ele;
+
+            if(map.containsKey(prefixSum-k)) {
+                count += map.get(prefixSum-k);
             }
-            
-            hm.put(prefixSum, hm.getOrDefault(prefixSum, 0) + 1); 
+
+            map.put(prefixSum, map.getOrDefault(prefixSum, 0) + 1);
         }
-        return noOfSubArrayPossible;
+
+        return count;
     }
 }
