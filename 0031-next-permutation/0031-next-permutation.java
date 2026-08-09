61aaa3b6280226ce2f52ea36844043e42fd3d2a5
Prakash Kumar <prakashk13624@gmail.com>
2026-08-09 19:32:33 +0530

    Time: 0 ms (100%), Space: 44.5 MB (70.53%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.java
+++ 0031-next-permutation/0031-next-permutation.java
@@ -1,37 +1,38 @@
 class Solution {
     public void nextPermutation(int[] nums) {
         int n = nums.length;
-        // Step-1: finding break Idx
-        int breakIdx = -1;
-        for(int i = n-2; i >= 0; i--) {
-            if(nums[i]<nums[i+1]) {
-                breakIdx = i;
+        // Finding first Small Idx
+        int smallIdx = -1;
+        for(int i = n-1; i > 0; i--) {
+            if(nums[i] > nums[i-1]) {
+                smallIdx = i-1;
                 break;
             }
         }
-        if(breakIdx == -1) {
+        // checking Edge cases
+        if(smallIdx == -1) {
             reverse(nums, 0, n-1);
             return;
         }
-        // Step-2: finding slightly greater Idx and swapping
-        for(int i = n-1; i >= breakIdx; i--) {
-            if(nums[i]>nums[breakIdx]) {
-                int temp = nums[i];
-                nums[i] = nums[breakIdx];
-                nums[breakIdx] = temp;
+        // finding slightly greater ele and swaping 
+        for(int i = n-1; i >= smallIdx; i--) {
+            if(nums[i] > nums[smallIdx]) {
+                int temp = nums[smallIdx];
+                nums[smallIdx] = nums[i];
+                nums[i] = temp;
                 break;
             }
         }
-        // Step-3: reverse breakIdx+1 to n-1
-        reverse(nums, breakIdx+1, n-1);
+        //reversing smallIdx+1 to n-1
+        reverse(nums, smallIdx+1, n-1);
     }
-    public void reverse(int[] nums, int a, int b) {
-        while(a < b) {
-            int temp = nums[a];
-            nums[a] = nums[b];
-            nums[b] = temp;
-            a++;
-            b--;
+    public void reverse(int[] nums, int i, int j) {
+        while(i < j) {
+            int temp = nums[i];
+            nums[i] = nums[j];
+            nums[j] = temp;
+            i++;
+            j--;
         }
     }
 }
