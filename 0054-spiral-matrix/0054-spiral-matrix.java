82214cdb0b7260eeaf3a0478b69a8ad05a536db9
Prakash Kumar <prakashk13624@gmail.com>
2026-08-10 17:16:07 +0530

    Time: 0 ms (100%), Space: 43.2 MB (9.63%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.java
+++ 0054-spiral-matrix/0054-spiral-matrix.java
@@ -1,30 +1,29 @@
 class Solution {
     public List<Integer> spiralOrder(int[][] matrix) {
-        int m = matrix.length;
-        int n = matrix[0].length;
-
+        int rows = matrix.length;
+        int columns = matrix[0].length;
         List<Integer> ans = new ArrayList<>();
 
-        int rs = 0, re = m-1;
-        int cs = 0, ce = n-1;
-        while(rs <= re && cs <= ce) {
+        int cs = 0, ce = columns-1;
+        int rs = 0, re = rows-1;
+        while(rs<=re && cs<=ce) {
             for(int i = cs; i <= ce; i++) {
                 ans.add(matrix[rs][i]);
             }
             rs++;
-            if(rs > re || cs > ce) break;
+            if(rs>re || cs>ce) break;
 
             for(int i = rs; i <= re; i++) {
                 ans.add(matrix[i][ce]);
             }
             ce--;
-            if(rs > re || cs > ce) break;
+            if(rs>re || cs>ce) break;
 
             for(int i = ce; i >= cs; i--) {
                 ans.add(matrix[re][i]);
             }
             re--;
-            if(rs > re || cs > ce) break;
+            if(rs>re || cs>ce) break;
 
             for(int i = re; i >= rs; i--) {
                 ans.add(matrix[i][cs]);
