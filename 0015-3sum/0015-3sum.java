a65899ac3ac884a3fdb039defa02b365448a2188
Prakash Kumar <prakashk13624@gmail.com>
2026-08-11 11:23:28 +0530

    Time: 31 ms (82.91%), Space: 59 MB (86.32%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,41 +1,40 @@
-class Solution 
-{
-    public List<List<Integer>> threeSum(int[] nums) 
-    {
-        List<List<Integer>> res = new ArrayList<>();
-        if (nums.length < 3) return res;
-
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        int n = nums.length;
+        List<List<Integer>> ans = new ArrayList<>();
+        // Shorting the array
         Arrays.sort(nums);
+        // 3-Sum logic
+        for(int i=0; i<n; i++) {
 
-        for (int i = 0; i < nums.length - 2; i++) 
-        {
-            if (i > 0 && nums[i] == nums[i - 1]) continue;
-            if (nums[i] > 0) break; 
+            if(i>0 && nums[i]==nums[i-1]) { // Skip duplicate first elements
+                continue;
+            }
 
-            int left = i + 1, right = nums.length - 1;
-            int target = -nums[i];
-
-            while (left < right) 
-            {
-                int sum = nums[left] + nums[right];
-                if (sum == target) 
-                {
-                    res.add(Arrays.asList(nums[i], nums[left], nums[right]));
-                    left++;
-                    right--;
-
-                    while (left < right && nums[left] == nums[left - 1]) left++;
-                    while (left < right && nums[right] == nums[right + 1]) right--;
-                } else if (sum < target) 
-                {
-                    left++;
-                } else 
-                {
-                    right--;
+            int j = i+1;
+            int k = n-1;
+            while(j < k) {
+                int sum = nums[i]+nums[j]+nums[k];
+                if(sum < 0) {
+                    j++;
+                } else if(sum > 0) {
+                    k--;
+                } else {
+                    ans.add(Arrays.asList(nums[i], nums[j], nums[k]));
+                    j++;
+                    k--;
+                    // Skip duplicates
+                    while(j<k && nums[j]==nums[j-1]) {
+                        j++;
+                    }
+                    while(j<k && nums[k]==nums[k+1]) {
+                        k--;
+                    }
                 }
             }
+
         }
 
-        return res;
+        return ans;
     }
 }
