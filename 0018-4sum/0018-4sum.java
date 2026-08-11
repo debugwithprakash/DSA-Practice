ccf00719b1fc6c708831c9d843fda5bf52b71a00
Prakash Kumar <prakashk13624@gmail.com>
2026-08-11 12:09:10 +0530

    Time: 19 ms (83.56%), Space: 45.8 MB (46.91%) - LeetHub

--- 0018-4sum/0018-4sum.java
+++ 0018-4sum/0018-4sum.java
@@ -1,46 +1,46 @@
 class Solution {
     public List<List<Integer>> fourSum(int[] nums, int target) {
-        List<List<Integer>> li=new ArrayList<>();
-        if(nums==null || nums.length<4){
-            return li;
-        }
+        int n = nums.length;
+        List<List<Integer>> ans = new ArrayList<>();
+        //Sorting array
         Arrays.sort(nums);
-        for(int i=0;i<nums.length-3;i++){
-            if(i>0&&nums[i]==nums[i-1]){
+        // 4-Sum logic
+        for(int i = 0; i < n; i++) {
+            // Skip duplicate first elements
+            if(i>0 && nums[i]==nums[i-1]) {
                 continue;
             }
-            for(int j=i+1;j<nums.length-2;j++)
-            {
-                if(j>i+1&&nums[j]==nums[j-1])
-                {
+
+            for(int j = i+1; j < n; j++) {
+                // Skip duplicate second elements
+                if(j>i+1 && nums[j]==nums[j-1]) {
                     continue;
                 }
-                int left=j+1; int right=nums.length-1;
-                while(left<right)
-                {
-                    long sum=(long)nums[i]+nums[j]+nums[left]+nums[right];
-                    if(sum==target)
-                    {
-                        li.add(Arrays.asList(nums[i],nums[j],nums[left],nums[right]));
-                        while(left<right&&nums[left]==nums[left+1])
-                        {
-                            left++;
+
+                int k = j+1;
+                int l = n-1;
+                while(k < l) {
+                    long sum = (long) nums[i]+nums[j]+nums[k]+nums[l];
+                    if(sum < target) {
+                        k++;
+                    } else if(sum > target) {
+                        l--;
+                    } else {
+                        ans.add(Arrays.asList(nums[i], nums[j], nums[k], nums[l]));
+                        k++;
+                        l--;
+                        // Skip duplicate third elements
+                        while(k<l && nums[k]==nums[k-1]) {
+                            k++;
                         }
-                        while(left<right&&nums[right]==nums[right+-1])
-                        {
-                            right--;
+                        // Skip duplicate fourth elements
+                        while(k<l && nums[l]==nums[l+1]) {
+                            l--;
                         }
-                        left++;
-                        right--;
-                    }else if(sum<target){
-                        left++;
-                    }else
-                    {
-                        right--;
                     }
                 }
             }
         }
-        return li;
+        return ans;
     }
 }
