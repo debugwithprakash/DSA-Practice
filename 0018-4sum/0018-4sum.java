65e8f674a0c639065357813110dfd81eed50c530
Prakash Kumar <prakashk13624@gmail.com>
2026-09-13 23:14:34 +0530

    Time: 19 ms (82.03%), Space: 45.7 MB (62.79%) - LeetHub

--- 0018-4sum/0018-4sum.java
+++ 0018-4sum/0018-4sum.java
@@ -1,40 +1,52 @@
 class Solution {
     public List<List<Integer>> fourSum(int[] nums, int target) {
+
         int n = nums.length;
+
         List<List<Integer>> ans = new ArrayList<>();
-        //Sorting array
+
         Arrays.sort(nums);
-        // 4-Sum logic
+
         for(int i = 0; i < n; i++) {
-            // Skip duplicate first elements
-            if(i>0 && nums[i]==nums[i-1]) {
+
+            if(i > 0 && nums[i] == nums[i-1]) {
                 continue;
             }
 
             for(int j = i+1; j < n; j++) {
-                // Skip duplicate second elements
-                if(j>i+1 && nums[j]==nums[j-1]) {
+
+                if(j > i+1 && nums[j] == nums[j-1]) {
                     continue;
                 }
 
                 int k = j+1;
                 int l = n-1;
+
                 while(k < l) {
-                    long sum = (long) nums[i]+nums[j]+nums[k]+nums[l];
+
+                    long sum = (long)nums[i]+nums[j]+nums[k]+nums[l];
+
                     if(sum < target) {
                         k++;
                     } else if(sum > target) {
                         l--;
                     } else {
-                        ans.add(Arrays.asList(nums[i], nums[j], nums[k], nums[l]));
+                        List<Integer> temp = new ArrayList<>();
+
+                        temp.add(nums[i]);
+                        temp.add(nums[j]);
+                        temp.add(nums[k]);
+                        temp.add(nums[l]);
+
+                        ans.add(temp);
+
                         k++;
                         l--;
-                        // Skip duplicate third elements
-                        while(k<l && nums[k]==nums[k-1]) {
+
+                        while(k < l && nums[k] == nums[k-1]) {
                             k++;
                         }
-                        // Skip duplicate fourth elements
-                        while(k<l && nums[l]==nums[l+1]) {
+                        while(k < l && nums[l] == nums[l+1]) {
                             l--;
                         }
                     }
