356481d26d7bce60259e5f25f2d0bf8f93812b29
Prakash Kumar <prakashk13624@gmail.com>
2026-09-17 22:13:53 +0530

    Time: 4 ms (12.22%), Space: 43.4 MB (32.33%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.java
+++ 0014-longest-common-prefix/0014-longest-common-prefix.java
@@ -3,18 +3,14 @@ class Solution {
         int n = strs.length;
 
         String ans = "";
-
         Arrays.sort(strs);
-
         char[] first = strs[0].toCharArray();
         char[] last = strs[n-1].toCharArray();
-
         for(int i=0; i<first.length; i++) {
             if(first[i] != last[i]) return ans;
-
             ans += first[i];
         }
-
+        
         return ans;
     }
 }
